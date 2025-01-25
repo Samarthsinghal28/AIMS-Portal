dab658655f1846f4f3633ce68637f4c41ac6e2b2
Ojas Jain <2022csb1099@iitrpr.ac.in>
2025-01-25 10:15:47 +0530

    resolved merge errors

--- frontend/src/app/admin/faculty_registeration/page.tsx
+++ frontend/src/app/admin/faculty_registeration/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { AdminDashboard } from "@/components/admin_dashboard";
 import { FacultyRegister } from "@/components/faculty-register";
 
@@ -12,18 +11,3 @@ export default function FacReg() {
     </div>
   );
 }
-=======
-import { AdminDashboard } from "@/components/admin_dashboard";
-import { FacultyRegister } from "@/components/faculty-register";
-
-export default function FacReg() {
-  return (
-    <div className="">
-      <AdminDashboard />
-      <div className="m-10 max-w-lg mx-auto bg-white p-6 rounded-lg shadow-md">
-        <FacultyRegister />
-      </div>
-    </div>
-  );
-}
->>>>>>> 882469471e3d2c11003a88e3b1b10ec648f3858a
